622b996f605730860c905751a2d50cd289b024cd
Kohei Watanabe <watanabe.kohei@gmail.com>
2026-09-05 05:46:42 +0900

    Ignore files

---
+++ src/kmeans.cpp
@@ -0,0 +1,35 @@
+#include <RcppArmadillo.h>
+#include <chrono>
+
+using namespace arma;
+using namespace std;
+using namespace Rcpp;
+
+inline std::vector<double> to_vector(const arma::urowvec& v) {
+  return arma::conv_to< std::vector<double> >::from(v);
+}
+
+// [[Rcpp::export]]
+List cpp_kmeans(arma::mat &data, int k, arma::mat &means,
+                int iter = 10, bool verbose = false) {
+
+  inplace_trans(data); // vectors are columns
+
+  bool status = kmeans(means, data, k, keep_existing,
+                       iter = iter, verbose = verbose);
+
+  if (!status)
+    throw std::runtime_error("Training of k-means failed");
+
+  return List::create(Rcpp::Named("k") = k,
+                      Rcpp::Named("centers") = means);
+}
+
+/*** R
+#mat <- matrix(rnorm(10000 * 10), ncol = 10)
+k <- 20
+m <- matrix(rnorm(ncol(mat) * k), ncol = k)
+out <- cpp_kmeans(mat, k, means = m, verbose = TRUE)
+out$cluster <- max.col(-1 * proxyC::dist(mat, t(out$centers), 1, sparse = FALSE) ^ 2)
+GMTM:::get_terms(out$cluster, dfmt, min_count = 1)
+*/
