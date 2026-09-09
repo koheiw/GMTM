56ef1f0d086394fbaddbc7d280d22e21552354fe
Kohei Watanabe <watanabe.kohei@gmail.com>
2026-09-09 09:21:30 +0900

    Improve test

--- tests/testthat/test-gmm.R
+++ tests/testthat/test-gmm.R
@@ -75,7 +75,7 @@ test_that("textmodel_gmm works", {
     docnames(dfmt_test)
   )
   expect_true(
-    all(round(rowSums(prob_ng), 10) %in% c(1, NA))
+    all(round(rowSums(prob_ng), 10) %in% c(1.0, NA_real_))
   )
   prob_gp <- probability(gmm_test, group = TRUE)
   expect_equal(
@@ -87,7 +87,7 @@ test_that("textmodel_gmm works", {
     levels(docid(dfmt_test))
   )
   expect_true(
-    all(round(rowSums(prob_gp), 10) %in% c(1, NA))
+    all(round(rowSums(prob_gp), 10) %in% c(1.0, NA_real_))
   )
   expect_error(
     probability(gmm_test, c("A")),
