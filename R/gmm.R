ededb66832d10604f30f84d7704285f637f5abf9
chainsawriot <chainsawtiney@gmail.com>
2026-09-11 13:59:07 +0200

    Fix partial matching +
    Also
    
    * rename `.RMD` to `Rmd` so that `devtools::build_readme()` works.
    * Also remove all minor issues to make `devtools::check()` works.
    * add a fixed random seed to README. But the function is still not
      stochastically stable. Will open another issue.

--- R/gmm.R
+++ R/gmm.R
@@ -27,7 +27,7 @@
 #'
 #' corp <- head(wordvector::data_corpus_news2014, 1000)
 #' toks <- tokens(corp, remove_punct = TRUE,
-#'                remove_symbols = TRUE, remove_number = TRUE) %>%
+#'                remove_symbols = TRUE, remove_numbers = TRUE) %>%
 #'         tokens_remove(stopwords("en"), min_nchar = 2)
 #' wov <- textmodel_word2vec(toks, dim = 50)
 #' dov <- as.textmodel_doc2vec(dfm(toks), wov)
